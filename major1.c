5fec3c1a25e95a970adcc7025c3882ba4a9679d8
SammyT1178 <61430214+SammyT1178@users.noreply.github.com>
2022-10-05 09:05:34 -0500

    Added palindrome function for case 4

--- major1.c
+++ major1.c
@@ -5,7 +5,6 @@
 
 #include<stdio.h>
 #include <stdbool.h> //Boolean values
-#include<stdlib.h> //Can be removed if there isn't any memory allocation
 
 int main(void)
 {
@@ -40,7 +39,7 @@ int main(void)
 				break;
 				
 			case 4: //Palindrome check
-				printf("Fuctionality not complete. Try again later.\n");
+				palindrome();
 				break;
 				
 			case 5: //Exit

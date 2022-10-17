d4eac69767358596d6f2dee16a6e5796612ab7eb
William Finch <williamfinch2@my.unt.edu>
2022-10-16 20:31:21 -0500

    Added some functionality

--- replace.c
+++ replace.c
@@ -1,7 +1,8 @@
 // Created by William Finch for Major Assignment 1 of CSCE 3600
 
 #include <stdio.h>
-#include "replace.h"
+#include <math.h>
+#include "major1.h"
 
 void main()
 {
@@ -12,12 +13,27 @@ void main()
 
 void replace()
 {
-    unsigned long int nonmask = -1;
+    unsigned long int number = 0;
     unsigned long int mask = 0;
+    int position = -1;
+
 
     printf("Please insert a positive integer less than 2,000,000,000: ");
-    scanf(" %ld", &nonmask);
-    printf("%ld\n", nonmask); // validates user input
+    scanf(" %lu", &number);
+    //printf("%lu\n", number); // validates user input
+
+    printf("Please insert a mask: ");
+    scanf(" %lu", &mask);
+    //printf("%lu\n", mask);
+
+    printf("Please input the bit to switch: ");
+    scanf(" %d", &position);
+    //printf("%d", position);
+
+    int maskPosition = pow(2, position);
+    number = number ^ ((number ^ mask) & maskPosition);
+    //printf("The number you have transformed with bit %d from mask %ld is %ld.\n", position, mask, number);
+
 
     return;
 }

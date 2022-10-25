48d8b938854edcfa6db3126076b180ea3ae71bec
Chaz Finch <williamfinch2@my.unt.edu>
2022-10-25 13:54:31 -0500

    Added to program

---
+++ power.c
@@ -0,0 +1,24 @@
+#include "major1.h"
+void powerOfTwo(int n)
+{
+int flag;
+flag=(n && (!(n&(n-1))));
+if(flag==1)
+{
+printf("%d is a power of 2\n",n);
+}
+else
+{
+int next = pow(2, ceil(log(n)/log(2)));
+printf("%d is not a power of 2\n",n);
+printf("Next higher integer that is power of 2 is %d\n",next);
+}
+}
+int main()
+{
+printf("Enter a positive integer less than 2 billion: ");
+int n;
+scanf("%d",&n);
+powerOfTwo(n);
+return 0;
+}
